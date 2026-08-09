248cd0960510cecbd8e381fa3ebfaa81eff84c6d
bruisedSoup <gonzales.isabella1414@gmail.com>
2026-08-09 22:52:02 +0800

    feat(users): add full_name property to User model

--- vista/users/models.py
+++ vista/users/models.py
@@ -63,8 +63,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
-    def get_full_name(self):
+    @property
+    def full_name(self):
         return f"{self.first_name} {self.last_name}".strip()
+
+    def get_full_name(self):
+        return self.full_name
 
     def get_short_name(self):
         return self.first_name
