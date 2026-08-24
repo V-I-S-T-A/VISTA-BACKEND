1180f5bf2e2d0479bbe45e47e56c27174061c3f2
Akane230 <dagoc.kharljey@gmail.com>
2026-08-24 18:44:11 +0800

    fix: remove the gunicorn from the entrypint so the docker will run the celery in the dockercompose

--- vista/entrypoint.sh
+++ vista/entrypoint.sh
@@ -26,7 +26,4 @@ PYEOF
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-exec gunicorn vista.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --timeout 120
+exec "$@"
